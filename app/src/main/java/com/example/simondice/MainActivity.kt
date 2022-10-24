e03af6ad1a55b95a41ce9f5abe18b84955373f42
David <dcounagogonzalez@danielcastelao.org>
2022-10-24 10:07:47 +0200

    Modificaciones en la clase comenzar, mostrar y selección

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.simondice
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import java.lang.Exception
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -12,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        comenzar()
     }
 
     var resultado : String = "Acierto"
@@ -19,71 +19,54 @@ class MainActivity : AppCompatActivity() {
     var select = ArrayList<String>()
     val start :Button = findViewById(R.id.Comenzar)
 
-    fun Comenzar(){
-        GenerarSecuencia()
-        if(resultado=="Acierto"){
+
+    fun comenzar(){
+        start.setOnClickListener { generarSecuencia() }
+        /*if(resultado=="Acierto"){
             GenerarSecuencia()
         }
         else{
          println("Has perdido, la secuencia era " + secuencia + " y tu selección fallaste en " + select + " \n Llegaste a la ronda: " + secuencia.size)
-        }
+         secuencia.clear()
+        }*/
     }
-
+    //Mëtodo random para la generación de secuencia
     fun Random.nextInt(range: IntRange): Int {
         return range.start + nextInt(range.last - range.start)
     }
 
-    val rojo : Button = findViewById(R.id.Rojo)
-    val amarillo : Button = findViewById(R.id.Amarillo)
-    val verde : Button = findViewById(R.id.Verde)
-    val azul : Button = findViewById(R.id.Azul)
 
-    fun Seleccion(){
-        rojo.setOnClickListener{
-            select.add("Rojo")
-        }
-        amarillo.setOnClickListener {
-            select.add("Amarillo")
-        }
-        verde.setOnClickListener{
-            select.add("Verde")
-        }
-        azul.setOnClickListener{
-            select.add("Azul")
-        }
-    }
-
-    fun GenerarSecuencia(){
-        var ronda : Int = secuencia.size
-        var colores = ArrayList<String>()
+    fun generarSecuencia(){
+        // var ronda : Int = secuencia.size
+        val colores = ArrayList<String>()
         colores.addAll(listOf("Rojo", "Azul", "Verde", "Amarillo"))
-        var random = Random()
+        val random = Random()
 
         println(random.nextInt(0..4))
         secuencia.add(colores[random.nextInt(0..4)])
         println(secuencia)
-
+        mostrar()
     }
 
-    fun Mostrar (){
+    fun mostrar (){
+
         for (i in 0..secuencia.size){
-            var muestra : String = secuencia[i]
-            try {
-                Thread.sleep(2*1000)
-            }
-            catch (e : Exception){
-                println(e)
-            }
+            val muestra : String = secuencia[i]
+
             if (muestra=="Rojo"){
+                start.text=""
                 start.setBackgroundColor(R.id.Rojo)
             }
             else if (muestra=="Amarillo"){
+                start.text=""
                 start.setBackgroundColor(R.id.Amarillo)
             }
             else if (muestra=="Verde"){
+                start.text=""
                 start.setBackgroundColor(R.id.Verde)
             }
             else if (muestra=="Azul"){
+                start.text=""
                 start.setBackgroundColor(R.id.Azul)
             }
             else{
@@ -92,8 +75,43 @@ class MainActivity : AppCompatActivity() {
             }
         }
         start.setBackgroundColor(R.id.Comenzar)
+        seleccion()
     }
 
-    //En la función de selección del usuario habilitar la pulsación de los colores
+    //DEclaración de los Botones de colores
+    val rojo : Button = findViewById(R.id.Rojo)
+    val amarillo : Button = findViewById(R.id.Amarillo)
+    val verde : Button = findViewById(R.id.Verde)
+    val azul : Button = findViewById(R.id.Azul)
+
+    fun seleccion(){
+        //Mientras la lista de nuestra selección sea distinta a la de la secuencia seguiremos presionando botones
+        while (select.size!=secuencia.size) {
+            start.text="Eligiendo"
+            //Habilitamos los botones para poder seleccionarlos
+            rojo.isEnabled
+            amarillo.isEnabled
+            verde.isEnabled
+            azul.isEnabled
+            //Según el que presionemos se añadirá a la lista select
+            rojo.setOnClickListener {
+                select.add("Rojo")
+            }
+            amarillo.setOnClickListener {
+                select.add("Amarillo")
+            }
+            verde.setOnClickListener {
+                select.add("Verde")
+            }
+            azul.setOnClickListener {
+                select.add("Azul")
+            }
+        }
+        println(select)
+        comprobar()
+    }
+    fun comprobar(){
+
+    }
 
 }
