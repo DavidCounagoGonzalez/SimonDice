d0e2b1d6243579ff5f43c106d49bb2127f7be8f0
David <dcounagogonzalez@danielcastelao.org>
2022-11-03 14:40:41 +0100

    Error en muestra de secuencia, se necesita pausar el bucle en selección.

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -3,22 +3,35 @@ package com.example.simondice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import kotlinx.coroutines.*
 import java.util.*
 import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
+    var secuencia = ArrayList<String>()
+    var select = ArrayList<String>()
+    lateinit var lose: TextView
+    lateinit var start :Button
+    //Declaración de los Botones de colores
+    lateinit var rojo : Button
+    lateinit var amarillo : Button
+    lateinit var verde : Button
+    lateinit var azul : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        lose= findViewById(R.id.Derrrota)
+        start= findViewById(R.id.Comenzar)
+        //Declaración de los Botones de colores
+        rojo = findViewById(R.id.Rojo)
+        amarillo= findViewById(R.id.Amarillo)
+        verde = findViewById(R.id.Verde)
+        azul = findViewById(R.id.Azul)
         start.setOnClickListener { generarSecuencia() }
     }
-
-    var resultado : String = "Acierto"
-    var secuencia = ArrayList<String>()
-    var select = ArrayList<String>()
-    val start :Button = findViewById(R.id.Comenzar)
 
     //Método random para la generación de secuencia
     fun Random.nextInt(range: IntRange): Int {
@@ -39,67 +52,89 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun mostrar (){
-        GlobalScope.launch(Dispatchers.Main) {
+        //Por alguna razón no se muestran los fondos en el botón central
+        println(secuencia.size)
+       // GlobalScope.launch(Dispatchers.Main) {
             for (i in 0..secuencia.size) {
+                if(i<secuencia.size) {
                 val muestra: String = secuencia[i]
-
-                if (muestra == "Rojo") {
-                    start.text = ""
-                    start.setBackgroundColor(R.id.Rojo)
-                } else if (muestra == "Amarillo") {
-                    start.text = ""
-                    start.setBackgroundColor(R.id.Amarillo)
-                } else if (muestra == "Verde") {
-                    start.text = ""
-                    start.setBackgroundColor(R.id.Verde)
-                } else if (muestra == "Azul") {
-                    start.text = ""
-                    start.setBackgroundColor(R.id.Azul)
-                } else {
-                    println("Esto no debería haber pasado")
-                    break
+                println(muestra)
+                println("Llegué")
+                    if (muestra == "Rojo") {
+                        start.text = ""
+                        start.setBackgroundColor(R.id.Rojo)
+                    } else if (muestra == "Amarillo") {
+                        start.text = ""
+                        start.setBackgroundColor(R.id.Amarillo)
+                    } else if (muestra == "Verde") {
+                        start.text = ""
+                        start.setBackgroundColor(R.id.Verde)
+                    } else if (muestra == "Azul") {
+                        start.text = ""
+                        start.setBackgroundColor(R.id.Azul)
+                    }
                 }
-                delay(2000L)
+                else{
+                    start.setBackgroundColor(R.id.Comenzar)
+                    seleccion()
+                }
+               // delay(2000L)
             }
-        }
-        start.setBackgroundColor(R.id.Comenzar)
-        seleccion()
+        //}
     }
 
-    //DEclaración de los Botones de colores
-    val rojo : Button = findViewById(R.id.Rojo)
-    val amarillo : Button = findViewById(R.id.Amarillo)
-    val verde : Button = findViewById(R.id.Verde)
-    val azul : Button = findViewById(R.id.Azul)
 
     fun seleccion(){
+        println("si¿?")
         //Mientras la lista de nuestra selección sea distinta a la de la secuencia seguiremos presionando botones
-        while (select.size!=secuencia.size) {
+        for (i in 0.. secuencia.size){
+        if (i<secuencia.size) {
             start.text="Eligiendo"
-            //Habilitamos los botones para poder seleccionarlos
-            rojo.isEnabled
-            amarillo.isEnabled
-            verde.isEnabled
-            azul.isEnabled
             //Según el que presionemos se añadirá a la lista select
             rojo.setOnClickListener {
                 select.add("Rojo")
+                println(select)
             }
             amarillo.setOnClickListener {
                 select.add("Amarillo")
+                println(select)
             }
             verde.setOnClickListener {
                 select.add("Verde")
+                println(select)
             }
             azul.setOnClickListener {
                 select.add("Azul")
+                println(select)
             }
         }
-        println(select)
-        comprobar()
+        else {
+            println(select)
+            comprobar()
+        }
+            //Necesito encontrar una manera de pausar el loop mientras el usuario no haya presionado el boton
+    }
     }
     fun comprobar(){
-
+        println("estoy aquí¿?")
+            for (i in 0..select.size) {
+                    if (secuencia[i]!=select[i]) {
+                        restart()
+                    }
+            }
+        generarSecuencia()
     }
+
+    fun restart() {
+        GlobalScope.launch(Dispatchers.Main) {
+            secuencia.clear()
+            select.clear()
+            lose.setText("HAS PERDIDO!!")
+            delay(5000L)
+            start.text = "Empezar!!"
+            lose.setText("Presiona el botón central para \n comenzar de nuevo.")
+        }
+    }
+
 
 }
